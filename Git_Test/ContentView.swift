2d9171b31513c44fa75ed0603ce90d78237cc6d5
Ameur <abensefia@hct.ac.ae>
2023-06-24 14:25:08 +0400

    Initial Commit

---
+++ Git_Test/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Git_Test
+//
+//  Created by abensefia on 24/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        
+            Text("Group Members are:")
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Git_Test/Git_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_TestApp.swift
+//  Git_Test
+//
+//  Created by abensefia on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Git_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
